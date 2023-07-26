7598aff966fcfc2f17e1779a024e1771e7c89397
Akshay Rasal (arasal) <asrasal47@gmail.com>
2023-07-26 17:38:28 +0200

    Update pipex_utils_bonus.c fixing the issues with wrong PATH

--- pipex_utils_bonus.c
+++ pipex_utils_bonus.c
@@ -12,30 +12,60 @@
 
 #include "pipex_bonus.h"
 
+static int	find_path(char **env)
+{
+	int		i;
+
+	i = 0;
+	while (env[i] && ft_strncmp(env[i], "PATH=/", 6) != 0)
+		i++;
+	if (!env[i])
+		return (-1);
+	return (i);
+}
+
+static void	set_path_helper(char **path, char ***path1, char ***cmd)
+{
+	int	i;
+
+	i = 0;
+	while ((*path1)[i] != NULL)
+	{
+		*path = ft_strjoin((*path1)[i++], "/");
+		*path = ft_strjoin(*path, *cmd[0]);
+		if (!access(*path, F_OK))
+			break ;
+		free(*path);
+	}
+	free_str(*path1);
+}
+
 char	*set_path(char **env, char *str, char ***cmd)
 {
 	char	**path1;
 	char	*path;
 	int		i;
 
-	path1 = ft_split(env[4], '=');
+	i = find_path(env);
+	if (i == -1)
+	{
+		*cmd = ft_split(str, ' ');
+		if (!access(*cmd[0], F_OK) && !access(*cmd[0], X_OK))
+		{
+			path = ft_strdup(*cmd[0]);
+			return (path);
+		}
+		return (NULL);
+	}
+	path1 = ft_split(env[i], '=');
 	path1 = ft_split(path1[1], ':');
 	*cmd = ft_split(str, ' ');
-	if (access(*cmd[0], F_OK) == 0)
+	if (!access(*cmd[0], F_OK))
 	{
 		free_str(path1);
 		return (*cmd[0]);
 	}
-	i = 0;
-	while (path1[i] != NULL)
-	{
-		path = ft_strjoin(path1[i++], "/");
-		path = ft_strjoin(path, *cmd[0]);
-		if (access(path, F_OK) == 0)
-			break ;
-		free(path);
-	}
-	free_str(path1);
+	set_path_helper(&path, &path1, cmd);
 	return (path);
 }
 
