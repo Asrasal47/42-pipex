c3102cd6133856696b6d0642374d062244a0486e
Akshay Rasal <arasal@1-D-5.42heilbronn.de>
2023-07-26 17:35:20 +0200

    Fixed issue with wrong Path

--- pipex_utils.c
+++ pipex_utils.c
@@ -6,11 +6,39 @@
 /*   By: arasal <arasal@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 03:23:47 by arasal            #+#    #+#             */
-/*   Updated: 2022/08/21 20:36:14 by arasal           ###   ########.fr       */
+/*   Updated: 2023/07/26 17:33:34 by arasal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static int	find_path(char **env)
+{
+	int		i;
+
+	i = 0;
+	while (env[i] && ft_strncmp(env[i], "PATH=/", 6) != 0)
+		i++;
+	if (!env[i])
+		return (-1);
+	return (i);
+}
+
+static void	set_path_helper(char **path, char ***path1, char ***cmd)
+{
+	int	i;
+
+	i = 0;
+	while ((*path1)[i] != NULL)
+	{
+		*path = ft_strjoin((*path1)[i++], "/");
+		*path = ft_strjoin(*path, *cmd[0]);
+		if (!access(*path, F_OK))
+			break ;
+		free(*path);
+	}
+	free_str(*path1);
+}
 
 char	*set_path(char **env, char *str, char ***cmd)
 {
@@ -18,24 +46,26 @@ char	*set_path(char **env, char *str, char ***cmd)
 	char	*path;
 	int		i;
 
-	path1 = ft_split(env[4], '=');
+	i = find_path(env);
+	if (i == -1)
+	{
+		*cmd = ft_split(str, ' ');
+		if (!access(*cmd[0], F_OK) && !access(*cmd[0], X_OK))
+		{
+			path = ft_strdup(*cmd[0]);
+			return (path);
+		}
+		return (NULL);
+	}
+	path1 = ft_split(env[i], '=');
 	path1 = ft_split(path1[1], ':');
 	*cmd = ft_split(str, ' ');
-	if (access(*cmd[0], F_OK) == 0)
+	if (!access(*cmd[0], F_OK))
 	{
 		free_str(path1);
 		return (*cmd[0]);
 	}
-	i = 0;
-	while (path1[i] != NULL)
-	{
-		path = ft_strjoin(path1[i++], "/");
-		path = ft_strjoin(path, *cmd[0]);
-		if (access(path, F_OK) == 0)
-			break ;
-		free(path);
-	}
-	free_str(path1);
+	set_path_helper(&path, &path1, cmd);
 	return (path);
 }
 
@@ -45,6 +75,7 @@ void	ft_exec(char *argv, char **env)
 	char	*path;
 
 	path = set_path(env, argv, &cmd);
+	ft_putstr_fd(argv, 1);
 	if (execve(path, cmd, env) == -1)
 		ft_error_cmd(cmd);
 }
